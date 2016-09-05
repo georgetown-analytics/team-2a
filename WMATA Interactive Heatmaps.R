30a68b4b72418fe2d116d21a9b011cfa617ae0db
micahmelling <micahmelling@users.noreply.github.com>
2016-09-04 20:44:59 -0400

    Update WMATA Interactive Heatmaps.R

--- WMATA Interactive Heatmaps.R
+++ WMATA Interactive Heatmaps.R
@@ -11,3 +11,5 @@ wmata1 <- read.csv("wmata_final.csv")
 head(wmata1)
 
 heatmaply(wmata1[-1], k_row = 3, k_col = 2, labRow = wmata1$Station, Colv = FALSE, scale_fill_gradient_fun = scale_colour_gradient()) %>% layout(margin = list(l = 140, b = 50))
+
+#Output: https://rpubs.com/micahmelling/185640
