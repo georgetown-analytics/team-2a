c673bb0821504a61fb937c91ca0ffb01fc7a1ab5
micahmelling <micahmelling@users.noreply.github.com>
2016-09-06 23:03:20 -0400

    Add files via upload

---
+++ Yellow_Line_Improved3.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 21:32:46 2016
+
+@author: Micah
+"""
+
+#Reduce severity of Tier 5 by 50%
+
+import random
+import simpy
+
+class EV:
+    def __init__(self, env):
+        self.env = env
+        self.metro_process = env.process(self.drive(env))
+        self.resume = env.event()
+        
+    def drive(self,env):
+        while True:
+            yield env.timeout(4860)
+            noise = random.normalvariate(150, 40)
+            yield env.timeout(noise)
+            
+            tier_1 = (0,0,0,0,0,0,0,0,4,4)
+            tier_1_1 = random.choice(tier_1)
+            yield env.timeout(tier_1_1)
+
+            tier_2 = (0,0,0,0,0,0,0,0,0,5)
+            tier_2_2 = random.choice(tier_2)
+            yield env.timeout(tier_2_2)           
+
+            tier_3 = (0,0,0,0,6,6,6,6,6,6)
+            tier_3_3 = random.choice(tier_3)
+            yield env.timeout(tier_3_3)  
+
+            tier_4 = (0,0,0,0,0,0,0,8,8,8)
+            tier_4_4 = random.choice(tier_4)
+            yield env.timeout(tier_4_4)            
+
+            tier_5 = (0,0,0,0,0,0,0,8,8,8)
+            tier_5_5 = random.choice(tier_5)
+            yield env.timeout(tier_5_5)    
+
+            print(env.now)
+            self.resume.succeed()
+            self.resume = env.event()
+        
+            
+env = simpy.Environment()
+ev = EV(env)
+env.run(until=1530900)
